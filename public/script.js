b2c8d97125c78a0b169ee7f610b157728acb8cd2
Harsh <yourdevharsh@gmail.com>
2026-04-08 16:05:12 +0530

    Tried globe view

--- public/script.js
+++ public/script.js
@@ -1,36 +1,33 @@
-const map = L.map("map").setView([0, 0], 2);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "© OpenStreetMap",
-}).addTo(map);
-
-L.geoJSON(myData).addTo(map);
-
-const map2 = new maplibregl.Map({
-  container: "map2",
+const map = new maplibregl.Map({
+  container: "map",
   style: "https://demotiles.maplibre.org/style.json",
   center: [0, 0],
   zoom: 1,
-  projection: 'globe'
+  projection: {
+    type: 'globe' 
+  }
 });
 
-map2.on("load", () => {
-  map2.addSource("my-custom-data", {
+map.on("load", () => {
+
+  map.setPaintProperty('background', 'background-color', '#4cbdff');
+
+  map.addSource("my-custom-data", {
     type: "geojson",
-    data: myData,
+    data: globeView,
   });
 
-  map2.addLayer({
+  map.addLayer({
     id: "geojson-fill",
     type: "fill",
     source: "my-custom-data",
     paint: {
-      "fill-color": "#ff0000",
-      "fill-opacity": 0.4,
+      "fill-color": "#000000",
+      "fill-opacity": 0,
     },
   });
 
-  map2.addLayer({
+  map.addLayer({
     id: "geojson-border",
     type: "line",
     source: "my-custom-data",
@@ -40,20 +37,9 @@ map2.on("load", () => {
     },
   });
 
-  const bounds = new maplibregl.LngLatBounds();
+  
 
-  myData.features.forEach((feature) => {
-    const coords = feature.geometry.coordinates;
-    coords.forEach((polygon) => {
-      polygon.forEach((ring) => {
-        ring.forEach((coord) => {
-          bounds.extend(coord);
-        });
-      });
-    });
-  });
-
-  map2.fitBounds(bounds, { padding: 50 });
+  map.fitBounds(bounds, { padding: 50 });
 });
 
-map2.addControl(new maplibregl.NavigationControl(), 'top-right');
+map.addControl(new maplibregl.NavigationControl(), 'top-right');
